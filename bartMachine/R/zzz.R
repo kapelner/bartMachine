1c8a618c8a42d3b4ef553a015af48217c4b40a34
jbleich-ubuntu <jbleich@omega.(none)>
2013-12-06 08:33:22 -0800

    cleanups for cran

--- bartMachine/R/zzz.R
+++ bartMachine/R/zzz.R
@@ -1,4 +1,8 @@
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to BART v1.0\n")
+.onLoad = function(libname, pkgname) {
+  #packageStartupMessage("Welcome to BART v1.0\n")
   assign("bartMachine_globals", new.env(), envir = parent.env(environment()))
+ 
+}
+.onAttach = function(libname, pkgname){
+  packageStartupMessage("Welcome to bartMachine\n")
 }
