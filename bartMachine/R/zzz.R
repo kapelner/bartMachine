599abc7bc05a85234dda7be29c585be15bc103f2
Adam Kapelner <kapelner@gmail.com>
2013-12-08 17:19:42 -0500

    added v1.0.1 to changelog and description

--- bartMachine/R/zzz.R
+++ bartMachine/R/zzz.R
@@ -1,7 +1,5 @@
 .onLoad = function(libname, pkgname) {
-  #packageStartupMessage("Welcome to BART v1.0\n")
   assign("bartMachine_globals", new.env(), envir = parent.env(environment()))
- 
 }
 .onAttach = function(libname, pkgname){
   packageStartupMessage("Welcome to bartMachine\n")
