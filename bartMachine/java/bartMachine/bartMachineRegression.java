d6e15e9ff274787e4ccfcdf0ecd1a99307f4dd72
Adam Kapelner <kapelner@gmail.com>
2014-09-05 12:03:08 -0400

    put Java source in a second location to satisfy CRAN policies

---
+++ bartMachine/java/bartMachine/bartMachineRegression.java
@@ -0,0 +1,18 @@
+package bartMachine;
+
+/**
+ * The class that is instantiated to build a Regression BART model
+ * 
+ * @author Adam Kapelner and Justin Bleich
+ *
+ */
+public class bartMachineRegression extends bartMachine_i_prior_cov_spec {
+	
+	/**
+	 * Constructs the BART classifier for regression.
+	 */
+	public bartMachineRegression() {		
+		super();
+	}
+
+}
