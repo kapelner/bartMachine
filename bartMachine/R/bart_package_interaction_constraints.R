280874199b7a7cc09a7a9f907fee2aab9ee2cec7
Adam Kapelner <kapelner@gmail.com>
2020-01-19 16:26:44 -0500

    beginning of additive bartmachine

---
+++ bartMachine/R/bart_package_interaction_constraints.R
@@ -0,0 +1,15 @@
+
+AdditiveBartMachine = function(constraints, ...){
+	X = list(...)$X
+	y = list(...)$y
+	
+	if (is.null(X) || is.null(y)){
+		stop("Both X and y must be specified to use the additive bart machine")
+	}
+	
+	
+}
+
+
+
+
